c872b113284253fdf4c5445b4609537f3009696d
Igorek <if7187069@gmail.com>
2025-08-08 17:53:31 +0300

    Adding localization

--- app/routers/get_transletions.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.localization.utils import get_translation
-
-router = APIRouter()
-
-@router.get("/api/translations/{locale}")
-async def get_translations(locale: str):
-    return get_translation(locale)
